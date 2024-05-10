4eb118745e46853595772a279d9e984453b0e46b
mrequejo <mrequejo@106-17.info2>
2024-05-10 14:25:26 +0200

    Viaje con sus atributos

--- ScrumViajes/src/clases/Viaje.java
+++ ScrumViajes/src/clases/Viaje.java
@@ -1,5 +1,51 @@
 package clases;
 
+/**
+ * Clase con la informacion sobre los viajes
+ */
 public class Viaje {
+
+	private String lugar;
+
+	private String fecha;
+
+	private double precio;
+
+	public Viaje(String lugar, int dia, int mes, int ano, double precio) {
+
+		if (lugar != null && !lugar.equals("")) {
+			this.lugar = lugar;
+		}
+		
+		//asignar el atributo fecha AQUI
+			//funcion que formatea la fecha
+		if(precio >= 0) {
+			this.precio = precio;
+		}
+		
+		
+	
+
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getLugar() {
+		return lugar;
+	}
 
 }
