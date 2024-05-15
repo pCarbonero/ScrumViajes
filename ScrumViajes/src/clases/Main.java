4b128f459a2336f21923aaaf93a268eb3591ce96
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-05-15 09:06:54 +0200

    UPD MAIN

--- ScrumViajes/src/clases/Main.java
+++ ScrumViajes/src/clases/Main.java
@@ -1,10 +1,117 @@
 package clases;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+	
+	//Inicializamos el escaner en estatico
+	static Scanner sc = new Scanner(System.in);
 
+	/**
+	 * Clase main de nuestra Agencia de viajes
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Inicialimos la variable opción
+		int opc = 0;
+		
+		//Lanzamos el metodo muestra menú
+		muestraMenu();
+		
+		try {
+			opc=sc.nextInt();
+			sc.nextLine();
+			elijeOpcion(opc);
+		} catch (InputMismatchException e) {
+			System.out.println("Valor no valido");
+			sc.nextLine();
+		}
+		
+		//Repetimos el bucle hasta que se seleccione la opcion 6
+		while (opc!=6) {
+			//Lanzamos el metodo muestra menú
+			muestraMenu();
+			try {
+				opc=sc.nextInt();
+				sc.nextLine();
+				elijeOpcion(opc);
+			} catch (InputMismatchException e) {
+				System.out.println("Valor no valido");
+				sc.nextLine();
+			}
+			
+		};
+		System.out.println("Saliendo...");
+		
+		//Cerramos el escaner
+		sc.close();
 	}
+	
+	/**
+	 * Metodo que muestra un menú con las opciones de nuestro programa
+	 */
+	public static void muestraMenu() {
+		System.out.println("========================");
+		System.out.println("-         Menú         -");
+		System.out.println("========================");
+		System.out.println("1. Lista de viajes");
+		System.out.println("2. Añadir viaje");
+		System.out.println("3. Modifica el viaje");
+		System.out.println("4. Elimina el viaje");
+		System.out.println("5. Guardar los cambios");
+		System.out.println("6. Salir");
+		System.out.println("-------------------------");
+	}
+	
+	/**
+	 * Metodo que entra en la opcion de nuestro menú
+	 */
+	public static void elijeOpcion(int opc) {
+		switch (opc) {
+		case 1->{
+			//Gestion.mostrarLista();
+		}
+		case 2->{
+			
+		}
+		case 3->{
+			
+		}
+		case 4->{
+			
+		}
+		case 5->{
+			
+		}
+		case 6->{
+			
+		}
+		default->{
+			
+		}
+		}
+	}
+	
+	/**
+	 * Metodo que envia el objeto con los datos necesarios
+	 * @return
+	 */
+	public static Viaje pasaObj() {
+		Viaje vj = new Viaje();
+		
+		try {
+			System.out.println("Dame el lugar del viaje");
+			vj.lugar=sc.nextLine();
+		} catch (InputMismatchException e) {
+			System.out.println("El tipo no es el correcto");
+		}
+		
+		
+		
+		return vj;
+	}
+	
+	
 
 }
