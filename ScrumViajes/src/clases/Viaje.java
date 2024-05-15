c1aadee8ba346629ef5970eed82b3fb8d57347b6
mrequejo <mrequejo@106-17.info2>
2024-05-15 08:50:10 +0200

    Viaje Revision 2

--- ScrumViajes/src/clases/Viaje.java
+++ ScrumViajes/src/clases/Viaje.java
@@ -40,39 +40,7 @@ public class Viaje {
 		}
 
 	}
-
-	/**
-	 * Valida que la fecha esté en fomato dd/mm/aaaa, si es incorrecta saca el
-	 * mensaje default del atributo fecha, puede dar una excepción si no se
-	 * introducen números en la fecha
-	 *
-	 * @param fecha Fecha del viaje
-	 * @return booleano para saber si la fecha tiene formato válido
-	 */
-	public boolean validaFecha(String fecha) {
-		String[] cadena = fecha.split("/");
-		boolean valida = false;
-		int dia;
-		int mes;
-		// Valida que cada parte de la fecha tenga la longitud que debe
-		if (cadena.length == 3) {
-			if (cadena[0].length() == 2 && cadena[1].length() == 2 && cadena[2].length() == 4) {
-				valida = true;
-			}
-		}
-		// Si tiene todo el formato que debe tener comprueba que los números de la fecha
-		// sean correctos
-		if (valida) {
-			dia = Integer.parseInt(cadena[0]);
-			mes = Integer.parseInt(cadena[1]);
-			Integer.parseInt(cadena[2]);
-			if ((dia < 1 || dia > 31) || (mes < 1 || mes > 12)) {
-				valida = false;
-			}
-		}
-		return valida;
-	}
-
+	
 	/**
 	 * Obtiene la fecha del Viaje
 	 * 
@@ -135,6 +103,39 @@ public class Viaje {
 
 		return cadena;
 	}
+
+	/**
+	 * Valida que la fecha esté en fomato dd/mm/aaaa, si es incorrecta saca el
+	 * mensaje default del atributo fecha, puede dar una excepción si no se
+	 * introducen números en la fecha
+	 *
+	 * @param fecha Fecha del viaje
+	 * @return booleano para saber si la fecha tiene formato válido
+	 */
+	public boolean validaFecha(String fecha) {
+		String[] cadena = fecha.split("/");
+		boolean valida = false;
+		int dia;
+		int mes;
+		// Valida que cada parte de la fecha tenga la longitud que debe
+		if (cadena.length == 3) {
+			if (cadena[0].length() == 2 && cadena[1].length() == 2 && cadena[2].length() == 4) {
+				valida = true;
+			}
+		}
+		// Si tiene todo el formato que debe tener comprueba que los números de la fecha
+		// sean correctos
+		if (valida) {
+			dia = Integer.parseInt(cadena[0]);
+			mes = Integer.parseInt(cadena[1]);
+			Integer.parseInt(cadena[2]);
+			if ((dia < 1 || dia > 31) || (mes < 1 || mes > 12)) {
+				valida = false;
+			}
+		}
+		return valida;
+	}
+
 
 	/**
 	 * Comprueba si dos viajes son iguales, lo son cuando el lugar y la fecha
