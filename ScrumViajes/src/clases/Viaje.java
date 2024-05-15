e38c6d65231f5d5364d350e0184435b7253e841e
mrequejo <mrequejo@106-17.info2>
2024-05-15 08:47:47 +0200

    Viaje Revisado

--- ScrumViajes/src/clases/Viaje.java
+++ ScrumViajes/src/clases/Viaje.java
@@ -5,72 +5,154 @@ package clases;
  */
 public class Viaje {
 
+	/**
+	 * Lugar a donde va el viaje
+	 */
 	private String lugar;
-
+	/**
+	 * Fecha del viaje
+	 */
 	private String fecha = "01/01/2000";
-
+	/**
+	 * Precio del viaje
+	 */
 	private double precio;
 
-	public Viaje(String lugar, int dia, int mes, int ano, double precio) {
+	/**
+	 * Constructor con los atributos de viaje
+	 * 
+	 * @param lugar  Lugar a donde va el viaje
+	 * @param fecha  Fecha del viaje
+	 * @param precio Precio que tendrá el viaje
+	 */
+	public Viaje(String lugar, String fecha, double precio) {
 
 		if (lugar != null && !lugar.equals("")) {
 			this.lugar = lugar;
 		}
-		
-		
-		this.fecha = formatoFecha(dia, mes, ano); 
-		
-		if(precio >= 0) {
+
+		if (validaFecha(fecha)) {
+			this.fecha = fecha;
+		}
+
+		if (precio >= 0) {
 			this.precio = precio;
 		}
-		
-		
-	
 
 	}
 
+	/**
+	 * Valida que la fecha esté en fomato dd/mm/aaaa, si es incorrecta saca el
+	 * mensaje default del atributo fecha, puede dar una excepción si no se
+	 * introducen números en la fecha
+	 *
+	 * @param fecha Fecha del viaje
+	 * @return booleano para saber si la fecha tiene formato válido
+	 */
+	public boolean validaFecha(String fecha) {
+		String[] cadena = fecha.split("/");
+		boolean valida = false;
+		int dia;
+		int mes;
+		// Valida que cada parte de la fecha tenga la longitud que debe
+		if (cadena.length == 3) {
+			if (cadena[0].length() == 2 && cadena[1].length() == 2 && cadena[2].length() == 4) {
+				valida = true;
+			}
+		}
+		// Si tiene todo el formato que debe tener comprueba que los números de la fecha
+		// sean correctos
+		if (valida) {
+			dia = Integer.parseInt(cadena[0]);
+			mes = Integer.parseInt(cadena[1]);
+			Integer.parseInt(cadena[2]);
+			if ((dia < 1 || dia > 31) || (mes < 1 || mes > 12)) {
+				valida = false;
+			}
+		}
+		return valida;
+	}
+
+	/**
+	 * Obtiene la fecha del Viaje
+	 * 
+	 * @return
+	 */
 	public String getFecha() {
 		return fecha;
 	}
 
+	/**
+	 * Cambia la fecha del Viaje
+	 * 
+	 * @param fecha
+	 */
 	public void setFecha(String fecha) {
-		this.fecha = fecha;
+		if (validaFecha(fecha)) {
+			this.fecha = fecha;
+		}
 	}
 
+	/**
+	 * Obtiene el precio del Viaje
+	 * 
+	 * @return
+	 */
 	public double getPrecio() {
 		return precio;
 	}
 
+	/**
+	 * Cambia el precio del viaje
+	 * 
+	 * @param precio
+	 */
 	public void setPrecio(double precio) {
-		this.precio = precio;
+		if (precio >= 0) {
+			this.precio = precio;
+		}
 	}
 
+	/**
+	 * Obtiene el lugar del viaje
+	 * 
+	 * @return
+	 */
 	public String getLugar() {
 		return lugar;
 	}
 
-	
-	public String formatoFecha(int dia, int mes, int ano)  {
-		
-		String fecha = "";
-		
-		if(dia>0 && dia>=31) {
-			if(dia<=9) {
-				
-			}
-			
-			fecha += dia + "/";	
-		}
-		if(mes>0 && mes<=12) {
-			fecha += mes + "/";
-		
-		}
-		if(ano > 2000) {
-			fecha += ano;
-		}
-		
-		return fecha;
-		
+	/**
+	 * To string con la información del viaje
+	 * 
+	 * @return String con la información
+	 */
+	@Override
+	public String toString() {
+		String cadena = "";
+
+		cadena += this.lugar + ",  " + this.fecha + ", " + this.precio + "\n";
+
+		return cadena;
 	}
-	
+
+	/**
+	 * Comprueba si dos viajes son iguales, lo son cuando el lugar y la fecha
+	 * coinciden
+	 * 
+	 * @return Booleano de comprobación, true si son iguales, false si no lo son
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		boolean iguales = false;
+
+		Viaje viaje = (Viaje) obj;
+
+		if (this.lugar.equals(viaje.lugar) && this.fecha.equals(viaje.fecha)) {
+			iguales = true;
+		}
+
+		return iguales;
+	}
+
 }
