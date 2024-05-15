756754f0a029bb3c05f1e9dd4966f8482f1ca816
pCarbonero <pablo.carbonero@iesnervion.es>
2024-05-15 09:13:43 +0200

    [upd] leer y guardar en el fichero

--- ScrumViajes/src/clases/GestionFichero.java
+++ ScrumViajes/src/clases/GestionFichero.java
@@ -13,8 +13,10 @@ public class GestionFichero {
 			String a;
 			String[] partes = new String[3];
 			while((a = br.readLine()) != null) {
+				//Viaje nuevoViaje = new Viaje(-,-,-);
 				partes = a.split(":");
-				//
+				//nuevoViaje = (partes[1], partes[2], parseInt(partes[3]));
+				//Gestion.addViaje(nuevoViaje);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
